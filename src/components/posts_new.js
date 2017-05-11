17d2f2d7ace186b714a8d5dc3e0913e5533b7335
M. Hassan Khurshid <mkshid@gmail.com>
2017-05-11 15:50:20 +0200

    Add title field

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+
+  renderTitleField(field){
+    return (
+      <div className="form-group">
+        <label>Title</label>
+        <input
+           className="form-control"
+           type="text"
+           {...field.input}
+           />
+      </div>
+    );
+  }
+
   render(){
     return(
-      <div>Posts New</div>
+      <form>
+        <Field
+           name="title"
+           component={this.renderTitleField}
+           />
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  // Name of the form
+  form: 'PostsNewForm'
+})(PostsNew);
