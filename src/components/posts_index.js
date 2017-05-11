2ed1608ffc7d5d54870faa23cbe52f3b8a1a931d
M. Hassan Khurshid <mkshid@gmail.com>
2017-05-11 12:47:32 +0200

    Show posts in posts index

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
+import _ from 'lodash';
+
 
 class PostsIndex extends Component {
   componentDidMount(){
     this.props.fetchPosts();
   }
 
+  renderPosts(){
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      );
+    });
+  }
+
   render(){
+    console.log(this.props.posts);
     return (
       <div>
-        Posts Index
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
 
 }
 
+function mapStateToProps({posts}){
+  return {posts};
+}
 
-export default connect(null, { fetchPosts })(PostsIndex);
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
