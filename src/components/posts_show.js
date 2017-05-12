469fb8b7499913b6ad9a4b5ee73d4413e007d40e
M. Hassan Khurshid <mkshid@gmail.com>
2017-05-12 10:45:32 +0200

    Show the post with postShow Component

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -11,9 +11,17 @@ class PostsShow extends Component {
   }
 
   render(){
+    const { post } = this.props;
+
+    if (!post){
+      return <div>Loading...</div>;
+    }
+
     return (
       <div>
-        Posts Show
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     );
   }
@@ -26,4 +34,4 @@ function mapStateToProps({ posts }, ownProps){
   return {post: posts[ownProps.match.params.id]};
 }
 
-export default connect(null, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
